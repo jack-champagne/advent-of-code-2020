a66bf5e0141da3d6efa5115858b3e549f7aa1337
Jack Champagne <jackchampagne.r@gmail.com>
2020-12-03 01:49:43 -0500

    Reformat file structure again

---
+++ 1-1-solver.py
@@ -0,0 +1,22 @@
+my_file = open('1.txt')
+
+line_list = my_file.read().split('\n')
+line_list.remove('')
+
+i_list = [];
+for elem in line_list:
+  i_list.append(int(elem))
+
+sub_list = []
+for elem in i_list:
+  sub_list.append(2020 - elem)
+
+ans = []
+
+for elem in sub_list:
+  if elem in i_list:
+    ans.append(elem)
+
+print(ans[0] * ans[1])
+
+
---
+++ 2-2-solver.py
@@ -0,0 +1,19 @@
+import re
+file = open('2.txt')
+
+lines = file.read()
+patt = re.compile(r'(\d+)[-](\d+)\s([a-z]):\s(.*)\n')
+matches = patt.findall(lines)
+
+valid_passwords = 0
+for match in matches:
+    pos1 = int(match[0])
+    pos2 = int(match[1])
+    char = match[2]
+    password = match[3]
+
+    # Didn't read full question lol, here's the XOR gate that we're looking for.
+    if (char == password[pos1 - 1] or char == password[pos2 - 1]) and not (char == password[pos1 - 1] and char == password[pos2 - 1]):
+        valid_passwords += 1
+
+print(valid_passwords)
